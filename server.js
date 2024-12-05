52526e9dc5d391ef964bc2e7e09a6996c1e8ba9a
Muhammad Shoaib <shoaibtatlahonly@gmail.com>
2024-12-05 20:20:00 +0500

    get

--- server.js
+++ server.js
@@ -27,3 +27,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('Server is up and running');
+});
